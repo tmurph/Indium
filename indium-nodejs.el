6159f7f4ec779454b9c62fcc76b4239e0fb007ab
Trevor Murphy <trevormurphy@google.com>
2021-02-20 03:11:08 -0800

    Rework the NodeJS server interaction to use a comint pattern.
    This refactor provides a couple of benefits:
    1. Interaction with the node app's runtime context.
    2. Much faster reads from the node process.  (The rewrite lets us hook
       into the comint machinery for filtering output, when in turn lets
       us use compiled top-level functions instead of runtime-generated
       closures.)

--- indium-nodejs.el
+++ indium-nodejs.el
@@ -33,14 +33,28 @@
 (require 'map)
 (require 'seq)
 (require 'subr-x)
+(require 'comint)
 
 (declare-function indium-client-connect "indium-client.el")
 (declare-function indium-client-disconnect "indium-client.el")
+(declare-function indium-quit "indium-interaction.el")
 
 (defcustom indium-nodejs-client-closes-server nil
   "When non-nil, closing the indium client also closes indium's inferior NodeJS server."
   :group 'indium-nodejs
   :type 'boolean)
+
+(defcustom indium-nodejs-server-prompt-regexp "^> "
+  "Regexp to recognize prompts in the NodeJS server repl."
+  :group 'indium-nodejs
+  :type 'string)
+
+(defvar indium-nodejs--project-file nil
+  "The full path to the current indium project's configuration file.")
+(defvar indium-nodejs--project-name nil
+  "The \"name\" parameter of the current indium project's configuration.")
+(defvar indium-nodejs--debugger-connected nil
+  "Non-nil if the indium client is currently connected to a NodeJS server.")
 
 (defun indium-launch-nodejs (conf)
   "Start a NodeJS process.
@@ -55,20 +69,23 @@ execution at the first statement."
     (unless .program
       (user-error "No NodeJS program specified in the .indium.json file"))
     (let* ((default-directory .resolvedRoot)
-	   (filter (indium-nodejs--process-filter-function conf))
            (command-with-flags (indium-nodejs--command-with-flags
                                 .program
                                 .args
                                 .inspect-brk
                                 .port))
-	   (process (make-process :name "indium-nodejs-process"
-				  :buffer "*node process*"
-				  :filter filter
-				  :command (list shell-file-name
-						 shell-command-switch
-						 command-with-flags))))
+           (buffer (make-comint-in-buffer "indium-nodejs-process" "*node process*"
+                                          shell-file-name nil
+                                          shell-command-switch command-with-flags))
+	   (process (get-buffer-process buffer)))
+      (add-function :after (process-sentinel process)
+                    #'indium-nodejs--maybe-stop-client)
       (message "Running node command \"%s\"" command-with-flags)
-      (switch-to-buffer (process-buffer process)))))
+      (setq indium-nodejs--project-file .projectFile
+            indium-nodejs--project-name .name)
+      (with-current-buffer buffer
+        (indium-nodejs-mode))
+      (switch-to-buffer buffer))))
 
 
 (defun indium-nodejs--command-with-flags (program args inspect-brk &optional port)
@@ -85,31 +102,38 @@ otherwise use Node's default port (9229)."
         (inspect-port-flag (if port (format " --inspect-port=%s" port) "")))
     (format "%s%s%s %s" program inspect-flag inspect-port-flag args)))
 
-(defun indium-nodejs--process-filter-function (conf)
-  "Return a process filter function for CONF.
+(defun indium-nodejs--maybe-stop-client (proc _msg)
+  "Stop the indium client and clean up interaction buffers when PROC dies.
 
-The function watches the process output to determine when to connect and
-disconnect from the process."
-  (let ((connected))
-    (lambda (process output)
-      ;; Append output to the process buffer
-      (with-current-buffer (process-buffer process)
-        (goto-char (point-max))
-        (insert output))
-      (cond
-       ((and (not connected)
-             (string-match-p "Debugger listening on" output))
-        ;; Node will keep outputting the "Debugger listening on" message after
-        ;; each deconnection, so only try to connect one.
-        (setq connected t)
-        (let-alist conf
-          (indium-client-connect (file-name-directory .projectFile) .name)))
-       ((string-match-p "Waiting for the debugger to disconnect" output)
-        ;; When Node receives a signal to exit, it first waits for all
-        ;; debuggers to disconnect before shutting down.  Watch for the
-        ;; "Waiting for the debugger to disconnect" message and then do so.
-        (setq connected nil)
-        (indium-client-disconnect))))))
+Intended to be run as part of the inferior NodeJS process sentinel."
+  (when (not (process-live-p proc))
+    (indium-quit)))
+
+(defun indium-nodejs--inferior-node-filter-escape-codes (output)
+  "Remove escape codes that the inferior NodeJS server emits."
+  (replace-regexp-in-string "\033\\[[0-9]+[GKJ]" "" output))
+
+(defun indium-nodejs--inferior-node-watch-for-debugger-status (output)
+  "Update the indium client connection status based on the inferior NodeJS output."
+  (cond
+   ((and (not indium-nodejs--debugger-connected)
+         (string-match-p "Debugger listening on" output))
+    ;; Node will keep outputting the "Debugger listening on" message after
+    ;; each deconnection, so only try to connect one.
+    (indium-client-connect (file-name-directory indium-nodejs--project-file)
+                           indium-nodejs--project-name)
+    (setq indium-nodejs--debugger-connected t))
+   ((string-match-p "Waiting for the debugger to disconnect" output)
+    ;; When Node receives a signal to exit, it first waits for all
+    ;; debuggers to disconnect before shutting down.  Watch for the
+    ;; "Waiting for the debugger to disconnect" message and then do so.
+    (indium-client-disconnect)
+    (setq indium-nodejs--debugger-connected nil)))
+  output)
+
+(defun indium-nodejs--clean-up-project-vars ()
+  (setq indium-nodejs--project-file nil
+        indium-nodejs--project-name nil))
 
 (defun indium-nodejs--maybe-close-nodejs ()
   "Stop indium's inferior NodeJS server process.
@@ -120,6 +144,24 @@ However, this does nothing if `indium-nodejs-client-closes-server' is nil."
            (buf (and proc (process-buffer proc))))
       (when (process-live-p proc) (kill-process proc))
       (when (buffer-live-p buf) (kill-buffer buf)))))
+
+(define-derived-mode indium-nodejs-mode comint-mode "Indium NodeJS"
+  "Major mode for interacting with Indium's inferior NodeJS process.
+
+TODO: mention major commands
+
+TODO: mention major customization options"
+  (set (make-local-variable 'comint-prompt-regexp)
+       indium-nodejs-server-prompt-regexp)
+  (set (make-local-variable 'paragraph-start) indium-nodejs-server-prompt-regexp)
+  (set (make-local-variable 'paragraph-separate)
+       (concat indium-nodejs-server-prompt-regexp "$"))
+  (set (make-local-variable 'comint-prompt-read-only) t)
+  (add-hook 'comint-preoutput-filter-functions
+            'indium-nodejs--inferior-node-filter-escape-codes nil 'local)
+  (add-hook 'comint-preoutput-filter-functions
+            'indium-nodejs--inferior-node-watch-for-debugger-status nil 'local)
+  (add-hook 'kill-buffer-hook 'indium-nodejs--clean-up-project-vars nil 'local))
 
 (add-hook 'indium-client-closed-hook #'indium-nodejs--maybe-close-nodejs t)
 
